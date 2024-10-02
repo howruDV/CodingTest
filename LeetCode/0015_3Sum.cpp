614317d97ac71e8fb3af6ddcaf4c35c78af9d843
DV <c1oudcat@naver.com>
2024-10-03 00:27:06 +0900

    refactor : leetcode 0015 left Ptr와 right Ptr의 합이 twoSum인 경우에만 중복 처리 - twoSum이 아니라면 정답이 아닌 세트이므로, 다음 while문에서 처리됨 (관계 없음) - twoSum이라면 정답인 세트이므로, 중복인 수는 정답에 중복 포함되게 됨 (관계 있으므로 중복 처리)

---
+++ LeetCode/0015_3Sum.cpp
@@ -0,0 +1,44 @@
+// LeetCode 0015. 3Sum
+// https://leetcode.com/problems/3sum/
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Time Complexity : O(n)
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> ans;
+		sort(nums.begin(), nums.end());
+
+		for (int i = 0; i < nums.size() - 2; ++i)
+		{
+			if (i > 0 && nums[i] == nums[i - 1])
+				continue;
+
+			int twoSum = -nums[i];
+			int leftPtr = i + 1;
+			int rightPtr = nums.size() - 1;
+
+			while (leftPtr < rightPtr)
+			{
+				if (nums[leftPtr] + nums[rightPtr] == twoSum)
+				{
+					ans.push_back(vector<int>{nums[i], nums[leftPtr], nums[rightPtr]});
+					leftPtr++;
+				}
+				else if (nums[leftPtr] + nums[rightPtr] < twoSum)
+				{
+					leftPtr++;
+				}
+				else
+				{
+					rightPtr--;
+				}
+			}
+		}
+
+		return ans;
+	}
+};
