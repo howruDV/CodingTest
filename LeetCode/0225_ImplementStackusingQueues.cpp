b9fb8395e491f2b068f8e2746dc7242c2c0ebc83
DV <c1oudcat@naver.com>
2024-10-08 04:28:42 +0900

    study : Leetcode 0225

---
+++ LeetCode/0225_ImplementStackusingQueues.cpp
@@ -0,0 +1,40 @@
+// LeetCode 0225. Implement Stack using Queues
+// https://leetcode.com/problems/implement-stack-using-queues/
+// using one queue;
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> stack;
+
+public:
+    MyStack() {
+    }
+
+    void push(int x) {
+        stack.push(x);
+
+        for (int i = 1; i < stack.size(); ++i)
+        {
+            stack.push(stack.front());
+            stack.pop();
+        }
+    }
+
+    int pop() {
+        int top = stack.front();
+        stack.pop();
+        return top;
+    }
+
+    int top() {
+        return stack.front();
+    }
+
+    bool empty() {
+        return stack.empty();
+    }
+};
