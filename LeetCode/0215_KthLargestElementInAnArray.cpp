af77a74549063d4be866279ff4451ba9b230605d
DV <c1oudcat@naver.com>
2024-10-11 03:25:51 +0900

    solve : Leetcode 0215

--- LeetCode/0215_KthLargestElementInAnArray.cpp
+++ LeetCode/0215_KthLargestElementInAnArray.cpp
@@ -1,21 +1,21 @@
-// LeetCode 215. Kth Largest Element in an Array
-// https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+// LeetCode 0215. Kth Largest Element in an Array
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+#include <vector>
 #include <queue>
 using namespace std;
 
-// Time Complexity O(n)
-// Space Complexity O(n)
+// Time Complexity O(nlogn)
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int ret;
-        priority_queue<int> heap = priority_queue<int>(nums.begin(), nums.end());
-        for (int i = 0; i < k; ++i)
+        priority_queue<int> Heap(nums.begin(), nums.end());
+
+        for (int i = 0; i < k - 1; ++i)
         {
-            ret = heap.top();
-            heap.pop();
+            Heap.pop();
         }
 
-        return ret;
+        return Heap.top();
     }
 };
